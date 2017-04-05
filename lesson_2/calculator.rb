a40d4f034d5af04873b606fd98139de64f732fb0
Tom Straszewski <tomek.straszewski@gmail.com>
2017-04-05 21:10:14 +0100

    fixed rubocop offences in calculator.rb

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,7 +3,6 @@
 # perform operation on two numbers
 # print result
 
-#Welcome message and ask for name until NOT empty string given
 def prompt(messege)
   puts "=>#{messege}"
 end
@@ -25,8 +24,9 @@ def operation_to_messege(op)
   end
 end
 
+# Welcome message and ask for name until NOT empty string given
 prompt('Welcome to Calculator! What is your name?')
-
+# keep asking for name until given NOT empty string
 name = ''
 loop do
   name = gets.chomp
@@ -42,11 +42,9 @@ name.capitalize!
 prompt("Hello! #{name}")
 puts ''
 
-prompt('Please enter two numbers to perform caluclation!')
-
-#this is our main loop
+# this is our main loop
 loop do
-  #keep asking for name until given NOT empty string
+  prompt('Please enter two numbers to perform caluclation!')
   prompt('Enter first number: ')
 
   number1 = nil
@@ -92,19 +90,19 @@ loop do
   end
 
   result = case operator
-  when '1'
-    number1 + number2
-  when '2'
-    number1 - number2
-  when '3'
-    number1 * number2
-  when '4'
-    number1.to_f / number2.to_f
-  end
+           when '1'
+             number1 + number2
+           when '2'
+             number1 - number2
+           when '3'
+             number1 * number2
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   prompt("#{operation_to_messege(operator)} the two numbers...")
   prompt("The result is #{result}")
 
   prompt("Do you want to calculate some more ? (Y/N)")
-  break if gets.chomp.downcase!='y'
+  break if gets.chomp.downcase != 'y'
 end
