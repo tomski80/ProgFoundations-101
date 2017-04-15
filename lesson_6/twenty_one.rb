79fd1758f4d905df30de42258ca92215871090ac
Tom Straszewski <tomek.straszewski@gmail.com>
2017-04-15 10:18:58 +0100

    twenty one updated

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,10 +1,9 @@
-require 'pry'
-# card game
 VALUES = { '2' => 2, '3' => 3, '4' => 4,
            '5' => 5, '6' => 6, '7' => 7,
            '8' => 8, '9' => 9, '10' => 10,
            'jack' => 10, 'queen' => 10, 'king' => 10,
-           'ace' => 'ace' }
+           'ace' => 0 }
+WIN = 21
 
 def prompt(message)
   puts "=> #{message}"
@@ -31,20 +30,16 @@ def deal_card(deck, hand)
 end
 
 def deal_2_cards_each(player_hand, dealer_hand, deck)
-  deal_card(deck, player_hand)
-  deal_card(deck, dealer_hand)
-  deal_card(deck, player_hand)
-  deal_card(deck, dealer_hand)
+  2.times do
+    deal_card(deck, player_hand)
+    deal_card(deck, dealer_hand)
+  end
 end
 
 def remove_suits(hand)
   hand.map do |card|
     card[0]
   end
-end
-
-def remove_aces(hand)
-  hand.reject { |card| card[0] == 'ace' }
 end
 
 def count_aces(hand)
@@ -66,12 +61,11 @@ end
 
 def cards_value_in(hand)
   aces_count = count_aces(hand)
-  hand = remove_aces(hand)
   num_values_in_hand = convert_values_to_num(hand)
   sum_in_hand = num_values_in_hand.inject(:+) # sum without aces
   # add aces value
   while aces_count > 0
-    sum_in_hand += if (sum_in_hand + 11) < 21
+    sum_in_hand += if (sum_in_hand + 11) < WIN
                      11
                    else
                      1
@@ -82,17 +76,51 @@ def cards_value_in(hand)
 end
 
 def bust?(hand)
-  cards_value_in(hand) > 21
+  cards_value_in(hand) > WIN
+end
+
+def who_won?(player_hand, dealer_hand)
+  player_total = cards_value_in(player_hand)
+  dealer_total = cards_value_in(dealer_hand)
+  if dealer_total > WIN
+    :dealer_bust
+  elsif player_total > WIN
+    :player_bust
+  elsif player_total > dealer_total
+    :player
+  elsif player_total < dealer_total
+    :dealer
+  else
+    :tie!
+  end
+end
+
+def show_total(player_hand, dealer_hand, show_dealer = false)
+  prompt 'Player have ' + cards_value_in(player_hand).to_s + ' in hand!'
+  if show_dealer
+    prompt 'Dealer have ' + cards_value_in(dealer_hand).to_s + ' in hand!'
+  end
 end
 
 def display_winner(player_hand, dealer_hand)
-  if cards_value_in(player_hand) > cards_value_in(dealer_hand)
+  winner = who_won?(player_hand, dealer_hand)
+  display_dealer = winner == :player_bust ? false : true
+  display_cards(player_hand, dealer_hand, display_dealer)
+
+  case winner
+  when :dealer_bust
+    prompt "Dealer bust! You won the game!"
+  when :player_bust
+    prompt "Bust! Dealer won the game!"
+  when :player
     prompt 'You won the game!'
-  elsif cards_value_in(player_hand) < cards_value_in(dealer_hand)
+  when :dealer
     prompt 'Dealer won the game!'
   else
     prompt "It's a Tie!"
   end
+
+  show_total(player_hand, dealer_hand, display_dealer)
 end
 
 def display_all_cards(hand)
@@ -105,9 +133,9 @@ def display_first_card(hand)
   prompt "#{hand[0][0]} of #{hand[0][1]}"
 end
 
-def display_cards(player_hand, dealer_hand, end_game = false)
+def display_cards(player_hand, dealer_hand, show_dealers = false)
   prompt 'DEALER CARDS'
-  if end_game
+  if show_dealers
     display_all_cards(dealer_hand)
   else
     display_first_card(dealer_hand)
@@ -136,7 +164,7 @@ loop do
     loop do
       choice = gets.chomp.downcase
       break if %w[h s].include?(choice[0])
-      prompt "That is not a valid choice! Please enter 'h' or 's'"
+      prompt "Sorry! Please enter 'h' or 's'"
     end
 
     if choice[0] == 'h'
@@ -161,16 +189,8 @@ loop do
     end
   end
 
-  display_cards(player_hand, dealer_hand, true)
-  if bust?(dealer_hand)
-    prompt "Dealer bust! You won the game!"
-  elsif bust?(player_hand)
-    prompt "Bust! Dealer won the game!"
-  else
-    display_winner(player_hand, dealer_hand)
-  end
-  prompt 'Player have ' + cards_value_in(player_hand).to_s + ' in hand!'
-  prompt 'Dealer have ' + cards_value_in(dealer_hand).to_s + ' in hand!'
+  display_winner(player_hand, dealer_hand)
+
   puts
   prompt 'Do you want to play again? (y/n)'
   play_again = gets.chomp.downcase
